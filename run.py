a627711b5f0588bed8f04de54e942012f08b4d8d
Piyush Raja <piyush1.bksc@gmail.com>
2022-04-15 21:28:30 +0530

    run.py modified

--- run.py
+++ run.py
@@ -1,6 +1,4 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from  flask import render_template
-app=Flask(__name__)
-db=SQLAlchemy(app)
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///market.db'
+from market import app
+if __name__=='__main__' :
+    app.run(debug=True)
+
