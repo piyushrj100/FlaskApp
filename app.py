7353ac7f0cde04c0fe8b44fa7e0bdaa55b6aa210
Piyush Raja <piyush1.bksc@gmail.com>
2022-04-15 21:12:38 +0530

    Some changes

--- app.py
+++ app.py
@@ -1,30 +1,30 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from  flask import render_template
-app=Flask(__name__)
-db=SQLAlchemy(app)
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///market.db'
+# from flask import Flask
+# from flask_sqlalchemy import SQLAlchemy
+# from  flask import render_template
+# app=Flask(__name__)
+# db=SQLAlchemy(app)
+# app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///market.db'
 
-class Item(db.Model) :
-    id=db.Column(db.Integer(), primary_key=True)
-    name=db.Column(db.String(length=30), nullable=False, unique=True)
-    price=db.Column(db.Integer(), nullable=False)
-    barcode=db.Column(db.String(length=12), nullable=False, unique=True)
-    description=db.Column(db.String(length=1024),nullable=False,unique=True)
+# class Item(db.Model) :
+#     id=db.Column(db.Integer(), primary_key=True)
+#     name=db.Column(db.String(length=30), nullable=False, unique=True)
+#     price=db.Column(db.Integer(), nullable=False)
+#     barcode=db.Column(db.String(length=12), nullable=False, unique=True)
+#     description=db.Column(db.String(length=1024),nullable=False,unique=True)
 
-    def __repr__(self) :
-        return f'Item {self.name}'
+#     def __repr__(self) :
+#         return f'Item {self.name}'
 
 
-@app.route("/")
-@app.route("/Home")
-def home_page():
-    return render_template('home.html')
+# @app.route("/")
+# @app.route("/Home")
+# def home_page():
+#     return render_template('home.html')
 
-@app.route('/market')
-def market_page():
-    items=Item.query.all()   
-    return render_template('market.html', items=items)
+# @app.route('/market')
+# def market_page():
+#     items=Item.query.all()   
+#     return render_template('market.html', items=items)
 
 # @app.route("/about")
 # def about_page():
