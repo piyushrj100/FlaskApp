4169392df6f87df951891e701f8f797af7f14f02
Piyush Raja <piyush1.bksc@gmail.com>
2022-04-17 00:24:41 +0530

    added login page

--- market/routes.py
+++ market/routes.py
@@ -1,7 +1,7 @@
 from market import app
 from flask import render_template, redirect, url_for, flash, get_flashed_messages
 from market.models import Item, User
-from market.forms import RegisterForm
+from market.forms import RegisterForm, LoginForm
 from market import db
 
 @app.route("/")
@@ -32,4 +32,9 @@ def register_page():
 
     
     return render_template('register.html', form=form)
+
+@app.route('/login',methods=['GET','POST']) 
+def login_page() :
+    form=LoginForm()
+    return render_template('login.html',form=form)
 
