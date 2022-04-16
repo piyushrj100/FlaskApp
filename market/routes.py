276725e4996443cf865d6d00a35a3db4337f394b
Piyush Raja <piyush1.bksc@gmail.com>
2022-04-16 21:32:32 +0530

    made changes to routes

--- market/routes.py
+++ market/routes.py
@@ -25,6 +25,11 @@ def register_page():
         db.session.add(user_to_create)
         db.session.commit()
         return redirect(url_for('market_page'))
+
+    if form.errors != {} : #If no errors from form validation
+        for err_msg in form.errors.values():
+            print(f'There was an error with creating a user :{err_msg}')
+
     
     return render_template('register.html', form=form)
 
