591f6f8738687766350923a0ef192dbd207ced73
Piyush Raja <piyush1.bksc@gmail.com>
2022-04-16 23:57:11 +0530

    added password hashing

--- market/__init__.py
+++ market/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from  flask import render_template
+from flask_bcrypt import Bcrypt
 app=Flask(__name__)
 db=SQLAlchemy(app)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///market.db'
 app.config['SECRET_KEY']='b23c721a5fd672162bb74a7e'
+bcrypt=Bcrypt(app)
 from market import routes
