6753c84dc79578e49089763824a96728879d8189
Piyush Raja <piyush1.bksc@gmail.com>
2022-04-16 16:25:54 +0530

    added some changes

--- market/__init__.py
+++ market/__init__.py
@@ -4,5 +4,5 @@ from  flask import render_template
 app=Flask(__name__)
 db=SQLAlchemy(app)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///market.db'
-
+app.config['SECRET_KEY']='b23c721a5fd672162bb74a7e'
 from market import routes
