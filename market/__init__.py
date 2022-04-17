7831851ccaf993a8f8adc0d18be532ab6f81ad53
Piyush Raja <piyush1.bksc@gmail.com>
2022-04-17 15:45:20 +0530

    fixed the errors

--- market/__init__.py
+++ market/__init__.py
@@ -3,12 +3,20 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
-app=Flask(__name__)
-db=SQLAlchemy(app)
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///market.db'
-app.config['SECRET_KEY']='b23c721a5fd672162bb74a7e'
-bcrypt=Bcrypt(app)
-login_manager=LoginManager(app)
-login_manager.login_view="login_page"
-login_manager.login_message_category="info"
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = 'b23c721a5fd672162bb74a7e'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login_page"
+login_manager.login_message_category = "info"
 from market import routes
+
+
+
+
+
+
+
+
