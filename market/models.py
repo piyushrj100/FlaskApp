5c7f671bee41732608adc671df6b1f4f4ec17cd9
Piyush Raja <piyush1.bksc@gmail.com>
2022-04-17 23:04:32 +0530

    made changes for item purchase

--- market/models.py
+++ market/models.py
@@ -31,6 +31,9 @@ class User(db.Model, UserMixin):
 
     def check_password_correction(self, attempted_password):
         return bcrypt.check_password_hash(self.password_hash, attempted_password)
+    
+    def can_purchase(self,item_obj) :
+        return self.budget >= item_obj.price
 
 class Item(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
