7774c60637a084dd8bf3b00717dfdabab4fe8c46
Piyush Raja <piyush1.bksc@gmail.com>
2022-04-17 23:15:01 +0530

    modified routes file

--- market/routes.py
+++ market/routes.py
@@ -22,10 +22,9 @@ def market_page():
         p_item_object=Item.query.filter_by(name=purchased_item).first()
         if p_item_object:
             if current_user.can_purchase(p_item_object):
-                p_item_object.owner = current_user.id
-                current_user.budget -= p_item_object.price
-                db.session.commit()
-                flash(f"Congratulations! You purchased {p_item_object.name} for {p_item_object.price}",category='success')
+                p_item_object.buy(current_user)
+                
+                flash(f"Congratulations! You purchased {p_item_object.name} for {p_item_object.price}$",category='success')
             else:
                 flash(f"Unfortunately, you don't have enough money to purchase {p_item_object.name}",category='danger')
         return redirect(url_for('market_page'))
